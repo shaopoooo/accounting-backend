5922f4c9c47ac32b3a1b8d2ad266c3878621cf66
webberHuang0330 <webberHuang@progressing.today>
2022-09-06 14:40:52 +0800

    mongo_init_v1

---
+++ config/init-mongo.js
@@ -0,0 +1,9 @@
+db.createUser(
+    {
+        user: _getEnv("MONGO_USER"),
+        pwd: cat(_getEnv("MONGO_PASSWORD_FILE")),
+        roles: [
+            "readWrite", "dbAdmin"
+        ]
+    }
+);
