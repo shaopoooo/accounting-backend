64e4f6dfdc8567af3621ff9923ab3a6b6e1c0df3
webberHuang0330 <webberHuang@progressing.today>
2022-09-29 18:33:01 +0800

    add error function

--- src/handler/user/index.spec.js
+++ src/handler/user/index.spec.js
@@ -1,16 +1,45 @@
 const user = {
   login: (httpRequest) => {
-    const { email, password } = httpRequest.body
+    if (!httpRequest || !httpRequest.body) return HttpCode.serverError()
 
-    if (!email) return { statusCode: 400 }
-    if (!password) return { statusCode: 400 }
+    const { email, password } = httpRequest.body
+    if (!email) return HttpCode.badRequest('email')
+    if (!password) return HttpCode.badRequest('password')
 
     return { statusCode: 200 }
   }
 }
 
+const HttpCode = {
+  badRequest: (paramName) => {
+    return {
+      statusCode: 400,
+      body: MissingParamError(paramName)
+    }
+  },
+  serverError: () => {
+    return { statusCode: 500 }
+  }
+}
+
+const MissingParamError = (paramName) => {
+  class CustomError extends Error {
+    constructor (paramName) {
+      super(`Missing param ${paramName}`)
+      this.name = 'MissingParamError'
+    }
+  }
+  return new CustomError(paramName)
+}
+
 describe('login Router', () => {
-  test('return if email is not provided', () => {
+  test('return 500 if httpRequest.body is not provided', () => {
+    const sut = user
+    const httpResponse = sut.login()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('return 400 if email is not provided', () => {
     const sut = user
     const httpRequest = {
       body: {
@@ -19,5 +48,18 @@ describe('login Router', () => {
     }
     const httpResponse = sut.login(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(MissingParamError('email'))
+  })
+
+  test('return 400 if password is not provided', () => {
+    const sut = user
+    const httpRequest = {
+      body: {
+        email: '123'
+      }
+    }
+    const httpResponse = sut.login(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(MissingParamError('password'))
   })
 })
